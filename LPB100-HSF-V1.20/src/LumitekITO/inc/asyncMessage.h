23b9937d5f244ff17ba3ff9891f9c673df03af47
Meiyusong <mys812@163.com>
2014-10-12 23:07:45 +0800

    优化消息链表

--- LPB100-HSF-V1.20/src/LumitekITO/inc/asyncMessage.h
+++ LPB100-HSF-V1.20/src/LumitekITO/inc/asyncMessage.h
@@ -9,15 +9,21 @@
 
 
 
-typedef struct MSG_NODE
+typedef struct msg_node
 {
-	U8 cmdData;
+	U16 cmdData;
 	BOOL bReback;
 	U16 snIndex;
-	U8* socketData;
-	struct MSG_NODE* pNodeNext;
+	U8* pData;
+	struct msg_node* pNodeNext;
 }MSG_NODE;
 
+
+typedef struct
+{
+	U8 noteCount;
+	MSG_NODE* firstNodePtr;
+}LIST_HEADER;
 
 typedef struct
 {
@@ -25,12 +31,33 @@ typedef struct
 	MSG_NODE* headerNext;
 }NODE_HEADER;
 
+
+typedef enum
+{
+	MSG_CMD_SET_GPIO_STATUS			= 0x01,
+	MSG_CMD_GET_GPIO_STATUS			= 0x02,
+	MSG_CMD_SET_ALARM_DATA			= 0x03,
+	MSG_CMD_GET_ALARM_DATA			= 0x04,
+	MSG_CMD_DELETE_ALARM_DATA		= 0x05,
+
+	MSG_CMD_FOUND_DEVICE			= 0x23,
+	MSG_CMD_LOCK_DEVICE				= 0x24,
+
+	MSG_CMD_SEARCH_SERVER_ADDR		= 0x41,
+	MSG_CMD_REQUEST_CONNECT			= 0x42,
+	
+	MSG_CMD_HEART_BEAT 				= 0x61,
+	MSG_CMD_QUARY_MODULE_INFO		= 0x62,
+	MSG_CMD_SET_MODULE_NAME			= 0x63,
+	MSG_CMD_MODULE_UPGRADE			= 0x65,
+	MSG_CMD_ENTER_SMART_LINK		= 0x66,
+}MESSAGE_CMD_TYPE;
 void USER_FUNC deviceMessageThread(void);
 
 MSG_NODE* USER_FUNC mallocNodeMemory(U16 dataSize);
 void USER_FUNC insertListNode(BOOL insetToHeader, MSG_NODE* pNode);
-static void USER_FUNC deleteNodeItem(MSG_NODE* pNode);
-void USER_FUNC deleteListNode(U16 snIndex, BOOL bReback);
+BOOL USER_FUNC deleteListNode(MSG_NODE* pNode);
+
 
 
 
--- LPB100-HSF-V1.20/src/LumitekITO/src/asyncMessage.c
+++ LPB100-HSF-V1.20/src/LumitekITO/src/asyncMessage.c
@@ -19,7 +19,23 @@
 
 
 
-static MSG_NODE* g_pHeader = NULL;
+//static MSG_NODE* g_pHeader = NULL;
+static LIST_HEADER g_list_header;
+hfthread_mutex_t g_message_mutex;
+
+
+
+
+static void USER_FUNC messageListInit(void)
+{
+	g_list_header.firstNodePtr = NULL;
+	g_list_header.noteCount = 0;
+	if((hfthread_mutext_new(&g_message_mutex)!= HF_SUCCESS))
+	{
+		HF_Debug(DEBUG_ERROR, "failed to create g_message_mutex");
+
+	}
+}
 
 
 
@@ -30,8 +46,8 @@ MSG_NODE* USER_FUNC mallocNodeMemory(U16 dataSize)
 	pNode = (MSG_NODE*)mallocSocketData(sizeof(MSG_NODE));
 	if(pNode != NULL)
 	{
-		pNode->socketData = (U8*)mallocSocketData(dataSize);
-		if(pNode->socketData == NULL)
+		pNode->pData = (U8*)mallocSocketData(dataSize);
+		if(pNode->pData == NULL)
 		{
 			FreeSocketData((U8*)pNode);
 			pNode = NULL;
@@ -41,23 +57,28 @@ MSG_NODE* USER_FUNC mallocNodeMemory(U16 dataSize)
 }
 
 
+
 void USER_FUNC insertListNode(BOOL insetToHeader, MSG_NODE* pNode)
 {
-	if(g_pHeader == NULL)
+	LIST_HEADER* listHeader = &g_list_header;
+	MSG_NODE* pTempNode;
+
+	hfthread_mutext_lock(g_message_mutex);
+	pTempNode = listHeader->firstNodePtr;
+	if(listHeader->noteCount == 0)
 	{
-		g_pHeader = pNode;
+		listHeader->firstNodePtr = pNode;
 		pNode->pNodeNext = NULL;
 	}
 	else
 	{
 		if(insetToHeader)
 		{
-			pNode->pNodeNext = g_pHeader->pNodeNext;
-			g_pHeader = pNode;
+			pNode->pNodeNext = pTempNode->pNodeNext;
+			listHeader->firstNodePtr = pNode;
 		}
 		else
 		{
-			MSG_NODE* pTempNode = g_pHeader;
 			while(pTempNode->pNodeNext != NULL)
 			{
 				pTempNode = pTempNode->pNodeNext;
@@ -66,61 +87,103 @@ void USER_FUNC insertListNode(BOOL insetToHeader, MSG_NODE* pNode)
 			pNode->pNodeNext = NULL;
 		}
 	}
+	listHeader->noteCount++;
+	hfthread_mutext_unlock(g_message_mutex);
 }
 
 
 
 static void USER_FUNC deleteNodeItem(MSG_NODE* pNode)
 {
-	if(pNode->socketData != NULL)
+	if(pNode->pData != NULL)
 	{
-		FreeSocketData(pNode->socketData);
+		FreeSocketData(pNode->pData);
 	}
 	FreeSocketData((U8*)pNode);
 }
 
 
-
-void USER_FUNC deleteListNode(U16 snIndex, BOOL bReback)
+BOOL USER_FUNC deleteListNode(MSG_NODE* pNode)
 {
-	MSG_NODE* pTempNode = g_pHeader;
+	LIST_HEADER* listHeader = &g_list_header;
+	MSG_NODE* curNode;
+	MSG_NODE* pTempNode;
+	BOOL ret = FALSE;
 
 
-	if(g_pHeader == NULL)
+	hfthread_mutext_lock(g_message_mutex);
+	curNode = listHeader->firstNodePtr;
+	if(curNode == NULL || pNode == NULL)
 	{
 		HF_Debug(DEBUG_ERROR, "meiyusong===> deleteListNode error no node to delete\n");
-		return;
+		hfthread_mutext_unlock(g_message_mutex);
+		return FALSE;
 	}
+	
 
-	if(pTempNode->pNodeNext == NULL)
+	
+	if(curNode == pNode)
 	{
-		if(pTempNode->snIndex == snIndex && pTempNode->bReback == bReback)
-		{
-			deleteNodeItem(pTempNode);
-		}
+		listHeader->firstNodePtr = NULL;
 	}
 	else
 	{
-		while(pTempNode->pNodeNext != NULL)
+		while(curNode != NULL)
 		{
-			if(pTempNode->pNodeNext->snIndex == snIndex && pTempNode->pNodeNext->bReback == bReback)
+			if(curNode == pNode)
 			{
-				pTempNode->pNodeNext = pTempNode->pNodeNext->pNodeNext;
-				deleteNodeItem(pTempNode);
+				pTempNode->pNodeNext = curNode->pNodeNext;
+				deleteNodeItem(pNode);
+				ret = TRUE;
 				break;
 			}
+			pTempNode = curNode;
+			curNode = curNode->pNodeNext;
 		}
 	}
+	if(ret)
+	{
+		listHeader->noteCount--;
+	}
+	else
+	{
+		HF_Debug(DEBUG_ERROR, "meiyusong===> deleteListNode not found deleteListNode \n");
+	}
+	hfthread_mutext_unlock(g_message_mutex);
+	return ret;
 }
 
 
 
 void USER_FUNC deviceMessageThread(void)
 {
+	LIST_HEADER* listHeader = &g_list_header;
+	MSG_NODE* curNode;
+
+
+	messageListInit();
+	hfthread_enable_softwatchdog(NULL,30); //Start watchDog
 	while(1)
 	{
-
 		//u_printf(" deviceMessageThread \n");
+		hfthread_reset_softwatchdog(NULL); //tick watchDog
+		
+		if(listHeader->noteCount !=0)
+		{
+			curNode = listHeader->firstNodePtr;
+
+
+			switch(curNode->cmdData)
+			{
+				case MSG_CMD_FOUND_DEVICE:
+					break;
+
+				default:
+					HF_Debug(DEBUG_ERROR, "meiyusong===> deviceMessageThread not found MSG  curNode->cmdData=0x%X\n", curNode->cmdData);
+					break;
+			}
+			deleteListNode(curNode);
+		}
 		msleep(100);
 	}
 }
